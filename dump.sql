8cc445cf6458465db0508b666d41749b9ea69377
Maxime <maximebarber@gmail.com>
2023-07-08 11:41:55 +0200

    7: Dumped DB

--- dump.sql
+++ dump.sql
@@ -17,6 +17,60 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Data for Name: conference; Type: TABLE DATA; Schema: public; Owner: app
+--
+
+COPY public.conference (id, city, year, international) FROM stdin;
+\.
+
+
+--
+-- Data for Name: comment; Type: TABLE DATA; Schema: public; Owner: app
+--
+
+COPY public.comment (id, conference_id, author, text, email, created_at, photo_filename) FROM stdin;
+\.
+
+
+--
+-- Data for Name: doctrine_migration_versions; Type: TABLE DATA; Schema: public; Owner: app
+--
+
+COPY public.doctrine_migration_versions (version, executed_at, execution_time) FROM stdin;
+DoctrineMigrations\\Version20230708092731	2023-07-08 11:30:09	61
+\.
+
+
+--
+-- Data for Name: messenger_messages; Type: TABLE DATA; Schema: public; Owner: app
+--
+
+COPY public.messenger_messages (id, body, headers, queue_name, created_at, available_at, delivered_at) FROM stdin;
+\.
+
+
+--
+-- Name: comment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app
+--
+
+SELECT pg_catalog.setval('public.comment_id_seq', 1, false);
+
+
+--
+-- Name: conference_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app
+--
+
+SELECT pg_catalog.setval('public.conference_id_seq', 1, false);
+
+
+--
+-- Name: messenger_messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app
+--
+
+SELECT pg_catalog.setval('public.messenger_messages_id_seq', 1, false);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
